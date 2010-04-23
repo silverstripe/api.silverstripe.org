2f0d4e1dcb215eb6c51122f35830199f27313769
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-04-23 02:41:54 +0000

    MINOR Renamed src folder to not being hidden, as publishsite ignores it by default
    git-svn-id: http://svn.silverstripe.com/projects/ss2api/trunk@103409 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- makedoc.sh
+++ makedoc.sh
@@ -1,21 +1,21 @@
-directorybase=".src/"
+directorybase="src/"
 ignore="_config.php,main.php,static-main.php,rewritetest.php,_register_database.php,index.php,install.php,Core.php,thirdparty/,lang/,tests/,*.js,*.css,*.yml,*.ss,*.jpg,*.gif,*.png,*.inc"
 output="HTML:frames:default"
 templatebase="templates"
 defaultargs="--ignore '$ignore'  --output '$output' --defaultpackagename sapphire"
 
 # trunk
-svn up .src/trunk
-nice phpdoc --directory .src/trunk --target trunk --title "SilverStripe trunk API Documentation" $defaultargs
+svn up src/trunk
+nice phpdoc --directory src/trunk --target trunk --title "SilverStripe trunk API Documentation" $defaultargs
 
 # 2.4
-svn up .src/2.4
-nice phpdoc --directory .src/2.4 --target 2.4 --title "SilverStripe 2.4 API Documentation" $defaultargs
+svn up src/2.4
+nice phpdoc --directory src/2.4 --target 2.4 --title "SilverStripe 2.4 API Documentation" $defaultargs
 
 # 2.3
-svn up .src/2.3
-nice phpdoc --directory .src/2.3 $output --target 2.3 --title "SilverStripe 2.3 API Documentation" $defaultargs
+svn up src/2.3
+nice phpdoc --directory src/2.3 $output --target 2.3 --title "SilverStripe 2.3 API Documentation" $defaultargs
 
 # 2.2
-svn up .src/2.2
-nice phpdoc --directory .src/2.2 $output --target 2.2 --title "SilverStripe 2.2 API Documentation" $defaultargs
+svn up src/2.2
+nice phpdoc --directory src/2.2 $output --target 2.2 --title "SilverStripe 2.2 API Documentation" $defaultargs
