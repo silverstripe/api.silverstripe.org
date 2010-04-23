a00bbd41fbdb602633b836c6eca6ea7b62c2c187
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-04-23 02:09:20 +0000

    MINOR using nice for phpdoc, added 2.2 target, custom title
    git-svn-id: http://svn.silverstripe.com/projects/ss2api/trunk@103408 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- makedoc.sh
+++ makedoc.sh
@@ -2,19 +2,20 @@ directorybase=".src/"
 ignore="_config.php,main.php,static-main.php,rewritetest.php,_register_database.php,index.php,install.php,Core.php,thirdparty/,lang/,tests/,*.js,*.css,*.yml,*.ss,*.jpg,*.gif,*.png,*.inc"
 output="HTML:frames:default"
 templatebase="templates"
-defaultargs="--ignore '$ignore'  --output '$output' --defaultpackagename sapphire --title \"SilverStripe API Documentation\""
-
-# prepare base directory
-svn up .src
+defaultargs="--ignore '$ignore'  --output '$output' --defaultpackagename sapphire"
 
 # trunk
 svn up .src/trunk
-phpdoc --directory .src/trunk --target trunk $defaultargs
+nice phpdoc --directory .src/trunk --target trunk --title "SilverStripe trunk API Documentation" $defaultargs
 
 # 2.4
 svn up .src/2.4
-phpdoc --directory .src/2.4 --target 2.4 $defaultargs
+nice phpdoc --directory .src/2.4 --target 2.4 --title "SilverStripe 2.4 API Documentation" $defaultargs
 
 # 2.3
 svn up .src/2.3
-phpdoc --directory .src/2.3 $output --target 2.3 $defaultargs
+nice phpdoc --directory .src/2.3 $output --target 2.3 --title "SilverStripe 2.3 API Documentation" $defaultargs
+
+# 2.2
+svn up .src/2.2
+nice phpdoc --directory .src/2.2 $output --target 2.2 --title "SilverStripe 2.2 API Documentation" $defaultargs
