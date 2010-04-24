3efed8b2ce9e178c421e1658611e0a1fe84c603a
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-04-24 00:29:46 +0000

    bla
    git-svn-id: http://svn.silverstripe.com/projects/ss2api/trunk@103441 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- makedoc.sh
+++ makedoc.sh
@@ -8,13 +8,13 @@ svn co http://svn.silverstripe.com/open/phpinstaller/trunk src/trunk
 nice phpdoc --directory src/trunk --target trunk --title "SilverStripe trunk API Documentation" $defaultargs
 
 # 2.4
-svn co http://svn.silverstripe.com/open/phpinstaller/2.4 src/2.4
+svn co http://svn.silverstripe.com/open/phpinstaller/branches/2.4 src/2.4
 nice phpdoc --directory src/2.4 --target 2.4 --title "SilverStripe 2.4 API Documentation" $defaultargs
 
 # 2.3
-svn co http://svn.silverstripe.com/open/phpinstaller/2.3 src/2.3
+svn co http://svn.silverstripe.com/open/phpinstaller/branches/2.3 src/2.3
 nice phpdoc --directory src/2.3 $output --target 2.3 --title "SilverStripe 2.3 API Documentation" $defaultargs
 
 # 2.2
-svn co http://svn.silverstripe.com/open/phpinstaller/2.2 src/2.2
+svn co http://svn.silverstripe.com/open/phpinstaller/branches/2.2 src/2.2
 nice phpdoc --directory src/2.2 $output --target 2.2 --title "SilverStripe 2.2 API Documentation" $defaultargs
