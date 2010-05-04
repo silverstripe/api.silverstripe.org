b21a5190d265be9cf93e7f0ef10ad7f21b7e1deb
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-05-04 22:25:44 +0000

    ENHANCEMENT Added modules to docs
    git-svn-id: http://svn.silverstripe.com/projects/ss2api/trunk@104073 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- makedoc.sh
+++ makedoc.sh
@@ -7,7 +7,7 @@ cd `dirname $0`
 
 # trunk
 svn co http://svn.silverstripe.com/open/phpinstaller/trunk src/trunk
-nice phpdoc --directory src/trunk/cms --target trunk --title "SilverStripe trunk API Documentation" $defaultargs
+nice phpdoc --directory src/trunk --target trunk --title "SilverStripe trunk API Documentation" $defaultargs
 
 # 2.4
 svn co http://svn.silverstripe.com/open/phpinstaller/branches/2.4 src/2.4
@@ -20,3 +20,59 @@ nice phpdoc --directory src/2.3 $output --target 2.3 --title "SilverStripe 2.3 A
 # 2.2
 svn co http://svn.silverstripe.com/open/phpinstaller/branches/2.2 src/2.2
 nice phpdoc --directory src/2.2 $output --target 2.2 --title "SilverStripe 2.2 API Documentation" $defaultargs
+
+# modules: auth_openid
+svn co http://svn.silverstripe.com/open/modules/auth_openid/trunk src/modules/auth_openid/trunk
+nice phpdoc --directory src/modules/auth_openid/trunk --target modules/auth_openid/trunk --title "SilverStripe auth_openid module trunk API Documentation" $defaultargs
+
+# modules: blog
+svn co http://svn.silverstripe.com/open/modules/blog/trunk src/modules/blog/trunk
+nice phpdoc --directory src/modules/blog/trunk --target modules/blog/trunk --title "SilverStripe blog module trunk API Documentation" $defaultargs
+
+# modules: cmsworkflow
+svn co http://svn.silverstripe.com/open/modules/cmsworkflow/trunk src/modules/cmsworkflow/trunk
+nice phpdoc --directory src/modules/cmsworkflow/trunk --target modules/cmsworkflow/trunk --title "SilverStripe cmsworkflow module trunk API Documentation" $defaultargs
+
+# modules: ecommerce
+svn co http://svn.silverstripe.com/open/modules/ecommerce/trunk src/modules/ecommerce/trunk
+nice phpdoc --directory src/modules/ecommerce/trunk --target modules/ecommerce/trunk --title "SilverStripe ecommerce module trunk API Documentation" $defaultargs
+
+# modules: forum
+svn co http://svn.silverstripe.com/open/modules/forum/trunk src/modules/forum/trunk
+nice phpdoc --directory src/modules/forum/trunk --target modules/forum/trunk --title "SilverStripe forum module trunk API Documentation" $defaultargs
+
+# modules: googlesitemaps
+svn co http://svn.silverstripe.com/open/modules/googlesitemaps/trunk src/modules/googlesitemaps/trunk
+nice phpdoc --directory src/modules/googlesitemaps/trunk --target modules/googlesitemaps/trunk --title "SilverStripe googlesitemaps module trunk API Documentation" $defaultargs
+
+# modules: mssql
+svn co http://svn.silverstripe.com/open/modules/mssql/trunk src/modules/mssql/trunk
+nice phpdoc --directory src/modules/mssql/trunk --target modules/mssql/trunk --title "SilverStripe mssql module trunk API Documentation" $defaultargs
+
+# modules: multiform
+svn co http://svn.silverstripe.com/open/modules/multiform/trunk src/modules/multiform/trunk
+nice phpdoc --directory src/modules/multiform/trunk --target modules/multiform/trunk --title "SilverStripe multiform module trunk API Documentation" $defaultargs
+
+# modules: postgresql
+svn co http://svn.silverstripe.com/open/modules/postgresql/trunk src/modules/postgresql/trunk
+nice phpdoc --directory src/modules/postgresql/trunk --target modules/postgresql/trunk --title "SilverStripe postgresql module trunk API Documentation" $defaultargs
+
+# modules: recaptcha
+svn co http://svn.silverstripe.com/open/modules/recaptcha/trunk src/modules/recaptcha/trunk
+nice phpdoc --directory src/modules/recaptcha/trunk --target modules/recaptcha/trunk --title "SilverStripe recaptcha module trunk API Documentation" $defaultargs
+
+# modules: subsites
+svn co http://svn.silverstripe.com/open/modules/subsites/trunk src/modules/subsites/trunk
+nice phpdoc --directory src/modules/subsites/trunk --target modules/subsites/trunk --title "SilverStripe subsites module trunk API Documentation" $defaultargs
+
+# modules: sqlite3
+svn co http://svn.silverstripe.com/open/modules/sqlite3/trunk src/modules/sqlite3/trunk
+nice phpdoc --directory src/modules/sqlite3/trunk --target modules/sqlite3/trunk --title "SilverStripe sqlite3 module trunk API Documentation" $defaultargs
+
+# modules: tagfield
+svn co http://svn.silverstripe.com/open/modules/tagfield/trunk src/modules/tagfield/trunk
+nice phpdoc --directory src/modules/tagfield/trunk --target modules/tagfield/trunk --title "SilverStripe tagfield module trunk API Documentation" $defaultargs
+
+# modules: userforms
+svn co http://svn.silverstripe.com/open/modules/userforms/trunk src/modules/userforms/trunk
+nice phpdoc --directory src/modules/userforms/trunk --target modules/userforms/trunk --title "SilverStripe userforms module trunk API Documentation" $defaultargs
