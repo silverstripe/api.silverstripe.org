45a1220b9f49ac3918d238a187d9fbe3fb1d36a6
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-05-06 20:44:16 +0000

    FEATURE Added anchor highlights
    git-svn-id: http://svn.silverstripe.com/projects/ss2api/trunk@104296 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- templates/Converters/HTML/Smarty/templates/default/templates/media/app.js
+++ templates/Converters/HTML/Smarty/templates/default/templates/media/app.js
@@ -20,5 +20,17 @@
 		$(':header').live('mouseout', function(e) {
 			$(this).find('a.anchor').hide();
 		});
+		
+		// Highlight currently selected anchor
+		// TODO This should be replaced by a "h3:target" css rule
+		// once we have valid HTML ids (anchor links currently contain $ and underscores)
+		if(document.location.href.match(/#(.*)/)) {
+			var anchor = RegExp.$1;
+			// Assumption: header element is on same level as anchor
+			if(anchor) $('a[name=' + anchor + ']').siblings(':header').addClass('anchor-highlight');
+			$('a[href^=#]').live('click', function(e) {
+				$('.anchor-highlight').removeClass('anchor-highlight');
+			});
+		}
 	});
 }(jQuery));
