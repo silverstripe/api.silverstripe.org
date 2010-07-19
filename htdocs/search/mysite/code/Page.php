eb292cf579a7b90a145187c4d4dd2273bc2a87e8
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-07-19 10:21:38 +0000

    ENHANCEMENT Added <link> elements to Page class
    git-svn-id: http://svn.silverstripe.com/projects/ss2api/trunk@108136 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- htdocs/search/mysite/code/Page.php
+++ htdocs/search/mysite/code/Page.php
@@ -10,32 +10,22 @@ class Page extends SiteTree {
 }
 class Page_Controller extends ContentController {
 
-	/**
-	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	 * permissions or conditions required to allow the user to access it.
-	 *
-	 * <code>
-	 * array (
-	 *     'action', // anyone can access this action
-	 *     'action' => true, // same as above
-	 *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	 *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	 * );
-	 * </code>
-	 *
-	 * @var array
-	 */
-	public static $allowed_actions = array (
-	);
-
 	public function init() {
 		parent::init();
 
-		// Note: you should use SS template require tags inside your templates 
-		// instead of putting Requirements calls here.  However these are 
-		// included so that our older themes still work
 		Requirements::themedCSS('layout'); 
 		Requirements::themedCSS('typography'); 
 		Requirements::themedCSS('form'); 
+		
+		// Add link elements
+		$opensearchLinkTemplate = '<link rel="search" type="application/opensearchdescription+xml" href="/opensearch/definition/?version=%s" title="%s" />';
+		$versions = SSAPIProperty::get_versions();
+		if($versions) foreach($versions as $version) {
+			Requirements::insertHeadTags(sprintf(
+				$opensearchLinkTemplate, 
+				Convert::raw2att($version),
+				'SilverStripe API ' . Convert::raw2att($version)
+			));
+		}
 	}
 }
