117ce9cad3909c97f30adc417daaec77796c6e9e
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-07-19 10:21:28 +0000

    MINOR Added unit test for importer
    git-svn-id: http://svn.silverstripe.com/projects/ss2api/trunk@108134 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ htdocs/search/mysite/tests/unit/SSAPIGotApiImporterTest.php
@@ -0,0 +1,29 @@
+<?php
+class SSAPIGotApiImporterTest extends SapphireTest {
+
+	function testImport() {
+		$c = new SSAPIGotApiImporter(BASE_PATH . '/mysite/tests/unit/SSAPIGotApiImporterTest_fixture.xml');
+		$ids = $c->run();
+		
+		$classes = DataObject::get('SSAPIProperty', '"Type" = \'class\'');
+		$this->assertEquals(2, $classes->Count());
+		$this->assertEquals(
+			array('TestClass1', 'TestClass2'),
+			$classes->column('Name')
+		);
+		
+		$properties = DataObject::get('SSAPIProperty', '"Type" = \'property\'');
+		$this->assertEquals(1, $properties->Count());
+		$this->assertEquals(
+			array('$prop'),
+			$properties->column('Name')
+		);
+		
+		$methods = DataObject::get('SSAPIProperty', '"Type" = \'method\'');
+		$this->assertEquals(2, $methods->Count());
+		$this->assertEquals(
+			array('testMethod1', 'testMethod2'),
+			$methods->column('Name')
+		);
+	}
+}
