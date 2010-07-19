0dc230f249e5bf3112b981134b3bf7ed361df304
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2010-07-19 10:23:47 +0000

    ENHANCEMENT Added director routes for /opensearch and /lookup ENHANCEMENT Added opensearch description to mysite/_config.php
    git-svn-id: http://svn.silverstripe.com/projects/ss2api/trunk@108157 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- htdocs/search/mysite/_config.php
+++ htdocs/search/mysite/_config.php
@@ -17,5 +17,12 @@ SSViewer::set_theme('ss2apisearch');
 SiteTree::enable_nested_urls();
 
 Director::addRules(51, array(
-	'opensearch/$Action/$ID/$OtherID' => 'SSAPISearchController'
+	'opensearch/$Action/$ID/$OtherID' => 'SSAPISearchController',
+	'lookup/$Action/$ID/$OtherID' => 'SSAPILookupController',
+	'globalsearch//$Action/$ID' => 'OpenSearchController' // this might be spun off to a new project
 ));
+
+Sphinx::$var_path = BASE_PATH . '/mysite/conf';
+
+OpenSearchController::register_description('ssapi_trunk', new OpenSearchDescription(Director::absoluteBaseURL() . '/opensearch/description/?version=trunk'));
+// OpenSearchController::register_description('ssdoc', new OpenSearchDescription('http://doc.silverstripe.org/lib/exe/opensearch.php'));
