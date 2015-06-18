797fcee51c78ea6f86e730ff3d27b03b3529ca88
Damian Mooyman <damian@silverstripe.com>
2015-06-18 18:00:16 +1200

    Fixed broken align to use custom forked version (that doesn't break on global methods). Added 3.2 to version list

--- makedoc.sh
+++ makedoc.sh
@@ -3,144 +3,65 @@
 # Move to the base folder
 cd $(dirname "$0");
 
-APIGEN_BIN=${APIGEN_BIN:-apigen}
+#=== FUNCTION ==================================================================
+# NAME: 		checkout
+# DESCRIPTION:	Checks out a specific branch of a module into a folder. Not
+#				particular good for taking up space, but at the moment separate
+#				folders for each version we need will do.
+#
+#				The master branch will checked out by default
+# PARAMETERS:
+#				$1 - module path on github (e.g silverstripe/sapphire.git)
+#				$2 - branch name (e.g 3.0)
+#				$3 - module name (e.g sapphire)
+#
+#===============================================================================
+function checkout {
+	if [ -d src/$2/$3/.git ]; then
+		(cd src/$2/$3 && git reset --hard HEAD && git pull)
+	else
+		git clone --depth=100 --depth=100 $1 src/$2/$3
+	fi
+}
+
+#=== FUNCTION ================================================================
+# NAME: 		generate
+# DESCRIPTION:	Generates APIGEN documentation
+# PARAMETERS:
+#				$1 - branch name (e.g. 3.0)
+#				$2 - Title (e.g. "SilverStripe master API Docs")
+#
+#===============================================================================
+function generate {
+	vendor/bin/apigen generate --config conf/apigen/apigen.neon --source src/$1 --destination htdocs/$1 --title \"$2\"
+}
 
 # master
-if [ -d src/master/cms/.git ]; then
-	(cd src/master/cms && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 --depth=100 git://github.com/silverstripe/silverstripe-cms.git src/master/cms
-fi
-if [ -d src/master/sapphire/.git ]; then
-	(cd src/master/sapphire && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 --depth=100 git://github.com/silverstripe/sapphire.git src/master/sapphire
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/master --destination htdocs/master --title "SilverStripe master API Docs"
+checkout 'git://github.com/silverstripe/silverstripe-cms.git' 'master' 'cms'
+checkout 'git://github.com/silverstripe/sapphire.git' 'master' 'sapphire'
+checkout 'git://github.com/silverstripe/silverstripe-siteconfig.git' 'master' 'siteconfig'
+checkout 'git://github.com/silverstripe-labs/silverstripe-reports.git' 'master' 'reports'
+generate 'master' 'SilverStripe master API Docs'
+
+# 3.2
+checkout 'git://github.com/silverstripe/silverstripe-cms.git' '3.2' 'cms'
+checkout 'git://github.com/silverstripe/sapphire.git' '3.2' 'sapphire'
+checkout 'git://github.com/silverstripe/silverstripe-siteconfig.git' '3.2' 'siteconfig'
+checkout 'git://github.com/silverstripe-labs/silverstripe-reports.git' '3.2' 'reports'
+generate '3.2' 'SilverStripe 3.2 API Docs'
 
 # 3.1
-if [ -d src/3.1/cms/.git ]; then
-	(cd src/3.1/cms && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 --branch 3.1 git://github.com/silverstripe/silverstripe-cms.git src/3.1/cms
-fi
-if [ -d src/3.1/sapphire/.git ]; then
-	(cd src/3.1/sapphire && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 --branch 3.1 git://github.com/silverstripe/sapphire.git src/3.1/sapphire
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/3.1 --destination htdocs/3.1 --title "SilverStripe 3.1 API Docs"
+checkout 'git://github.com/silverstripe/silverstripe-cms.git' '3.1' 'cms'
+checkout 'git://github.com/silverstripe/sapphire.git' '3.1' 'sapphire'
+generate '3.1' 'SilverStripe 3.1 API Docs'
 
 # 3.0
-if [ -d src/3.0/cms/.git ]; then
-	(cd src/3.0/cms && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 --branch 3.1 git://github.com/silverstripe/silverstripe-cms.git src/3.0/cms
-fi
-if [ -d src/3.0/sapphire/.git ]; then
-	(cd src/3.0/sapphire && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 --branch 3.0 git://github.com/silverstripe/sapphire.git src/3.0/sapphire
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/3.0 --destination htdocs/3.0 --title "SilverStripe 3.0 API Docs"
+checkout 'git://github.com/silverstripe/silverstripe-cms.git' '3.0' 'cms'
+checkout 'git://github.com/silverstripe/sapphire.git' '3.0' 'sapphire'
+generate '3.0' 'SilverStripe 3.0 API Docs'
 
 # 2.4
-if [ -d src/2.4/cms/.git ]; then
-	(cd src/2.4/cms && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 --branch 2.4 git://github.com/silverstripe/silverstripe-cms.git src/2.4/cms
-fi
-if [ -d src/2.4/sapphire/.git ]; then
-	(cd src/2.4/sapphire && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 --branch 2.4 git://github.com/silverstripe/sapphire.git src/2.4/sapphire
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/2.4 --destination htdocs/2.4 --title "SilverStripe 2.4 API Docs"
+checkout 'git://github.com/silverstripe/silverstripe-cms.git' '2.4' 'cms'
+checkout 'git://github.com/silverstripe/sapphire.git' '2.4' 'sapphire'
+generate '2.4' 'SilverStripe 2.4 API Docs'
 
-# modules: blog
-if [ -d src/modules/blog/master/.git ]; then
-	(cd src/modules/blog/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe/silverstripe-blog.git src/modules/blog/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/blog/master --destination htdocs/modules/blog/master --title "SilverStripe blog module trunk API Docs"
-
-# modules: cmsworkflow
-if [ -d src/modules/cmsworkflow/master/.git ]; then
-	(cd src/modules/cmsworkflow/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe/silverstripe-cmsworkflow.git src/modules/cmsworkflow/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/cmsworkflow/master --destination htdocs/modules/cmsworkflow/master --title "SilverStripe cmsworkflow module trunk API Docs"
-
-# modules: forum
-if [ -d src/modules/forum/master/.git ]; then
-	(cd src/modules/forum/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe/silverstripe-forum.git src/modules/forum/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/forum/master --destination htdocs/modules/forum/master --title "SilverStripe forum module trunk API Docs"
-
-# modules: mssql
-if [ -d src/modules/mssql/master/.git ]; then
-	(cd src/modules/mssql/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe/silverstripe-mssql.git src/modules/mssql/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/mssql/master --destination htdocs/modules/mssql/master --title "SilverStripe mssql module trunk API Docs"
-
-# modules: multiform
-if [ -d src/modules/multiform/master/.git ]; then
-	(cd src/modules/multiform/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe/silverstripe-multiform.git src/modules/multiform/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/multiform/master --destination htdocs/modules/multiform/master --title "SilverStripe multiform module trunk API Docs"
-
-# modules: postgresql
-if [ -d src/modules/postgresql/master/.git ]; then
-	(cd src/modules/postgresql/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe/silverstripe-postgresql.git src/modules/postgresql/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/postgresql/master --destination htdocs/modules/postgresql/master --title "SilverStripe postgresql module trunk API Docs"
-
-# modules: recaptcha
-if [ -d src/modules/recaptcha/master/.git ]; then
-	(cd src/modules/recaptcha/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/chillu/silverstripe-recaptcha.git src/modules/recaptcha/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/recaptcha/master --destination htdocs/modules/recaptcha/master --title "SilverStripe recaptcha module trunk API Docs"
-
-# modules: subsites
-if [ -d src/modules/subsites/master/.git ]; then
-	(cd src/modules/subsites/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe/silverstripe-subsites.git src/modules/subsites/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/subsites/master --destination htdocs/modules/subsites/master --title "SilverStripe subsites module trunk API Docs"
-
-# modules: sqlite3
-if [ -d src/modules/sqlite3/master/.git ]; then
-	(cd src/modules/sqlite3/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe-labs/silverstripe-sqlite3.git src/modules/sqlite3/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/sqlite3/master --destination htdocs/modules/sqlite3/master --title "SilverStripe sqlite3 module trunk API Docs"
-
-# modules: tagfield
-if [ -d src/modules/tagfield/master/.git ]; then
-	(cd src/modules/tagfield/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/chillu/silverstripe-tagfield.git src/modules/tagfield/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/tagfield/master --destination htdocs/modules/tagfield/master --title "SilverStripe tagfield module trunk API Docs"
-
-# modules: userforms
-if [ -d src/modules/userforms/master/.git ]; then
-	(cd src/modules/userforms/master && git reset --hard HEAD && git pull)
-else
-	git clone --depth=100 git://github.com/silverstripe/silverstripe-userforms.git src/modules/userforms/master
-fi
-nice $APIGEN_BIN --config conf/apigen/apigen.neon --source src/modules/userforms/master --destination htdocs/modules/userforms/master --title "SilverStripe userforms module trunk API Docs"
