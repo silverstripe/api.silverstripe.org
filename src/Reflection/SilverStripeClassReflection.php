90fd858cd6500d8591782b9f0650d2c3488c0956
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2023-05-02 15:20:09 +1200

    FIX Correctly display config as its own section
    There was code there to do this already, but it seems like it just never
    got finished.

--- src/Reflection/SilverStripeClassReflection.php
+++ src/Reflection/SilverStripeClassReflection.php
@@ -7,18 +7,39 @@ use Doctum\Reflection\PropertyReflection;
 
 class SilverStripeClassReflection extends ClassReflection
 {
-
     /** @var array<string,PropertyReflection> */
     protected $configs = [];
 
     public function addConfig(PropertyReflection $property): void
     {
         $this->configs[$property->getName()] = $property;
-        $property->setClass($this);
     }
 
-    public function getConfigs(): array
+    public function getConfigs($deep = false): array
     {
+        if (empty($this->configs) && $this->hasTrait('SilverStripe\Core\Config\Configurable')) {
+            foreach ($this->getProperties($deep) as $property) {
+                if ($this->propertyIsConfig($property)) {
+                    $this->addConfig($property);
+                }
+            }
+        }
         return $this->configs;
+    }
+
+    public function propertyIsConfig(PropertyReflection $property): bool
+    {
+        return !$property->isInternal() && $property->isStatic() && $property->isPrivate();
+    }
+
+    private function hasTrait(string $traitName): bool
+    {
+        $traits = $this->getTraits(true);
+        foreach ($traits as $trait) {
+            if ($trait->getName() === $traitName) {
+                return true;
+            }
+        }
+        return false;
     }
 }
