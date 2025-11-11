179c275a861465e01f0ff5619624495a13b28c4c
Steve Boyd <emteknetnz@gmail.com>
2025-11-11 17:41:30 +1300

    DEP Upgrade to CMS 6.1

--- app/src/Build/BuildsDocsQueuedJob.php
+++ app/src/Build/BuildsDocsQueuedJob.php
@@ -2,8 +2,11 @@
 
 namespace SilverStripe\ApiDocs\Build;
 
-use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
 use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\PolyExecution\PolyOutput;
+use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 /**
  * Queued job to build the API documentation
@@ -20,8 +23,10 @@ class BuildDocsQueuedJob extends AbstractQueuedJob
 
     public function process()
     {
-        $task = new BuildDocsTask($this);
-        $task->run(null);
+        $task = BuildDocsTask::create();
+        $input = new ArrayInput([]);
+        $output = PolyOutput::create(PolyOutput::FORMAT_ANSI, ConsoleOutput::VERBOSITY_VERBOSE);
+        $task->run($input, $output);
         $this->isComplete = true;
     }
 }
